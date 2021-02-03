830cbf6f80f1bbc34ccc51fd84743006a1ca876b
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-03 10:18:00 +0530

    Update ****(Kadane's Variation)Max Circular Subarray Sum.cpp

--- arrays/****(Kadane's Variation)Max Circular Subarray Sum.cpp
+++ arrays/****(Kadane's Variation)Max Circular Subarray Sum.cpp
@@ -33,3 +33,41 @@ int circularSubarraySum(int arr[], int num)
     return max(x,sum+y);
     // your code here
 }
+
+///////////////***** OR ******////////////////
+
+int circularSubarraySum(int arr[], int n)
+{
+    if(n==1)
+    {
+        return arr[0];
+    }
+    
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    
+    int cmax=arr[0];
+    int resmax=arr[0];
+    int cmin=arr[0];
+    int resmin=arr[0];
+    
+    for(int i=1;i<n;i++)
+    {
+        cmax=max(cmax+arr[i],arr[i]);
+        resmax=max(resmax,cmax);
+        
+        cmin=min(cmin+arr[i],arr[i]);
+        resmin=min(resmin,cmin);
+    }
+    
+    if(sum==resmin)
+    {
+        return resmax;
+    }
+    
+    return max(resmax,(sum-resmin));
+    // your code here
+}
