836a2c38aa9dd641404768e696dc34be3c5f6d18
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-03 22:31:34 +0530

    Update Pythagorean Triplet.cpp

--- Maths/Pythagorean Triplet.cpp
+++ Maths/Pythagorean Triplet.cpp
@@ -4,6 +4,34 @@ public:
 	// Pythagorean triplet exists or not
 	bool checkTriplet(int arr[], int n) 
 	{
+		sort(arr,arr+n);
+	    reverse(arr,arr+n);
+	    for(int i=0;i<=n-3;i++)
+	    {
+	        int low=i+1;
+	        int end=n-1;
+	        int num=arr[i]*arr[i];
+	        while(low<end)
+	        {
+	            int check=(arr[low]*arr[low])+(arr[end]*arr[end]);
+	            if(num==check)
+	            {
+	                return true;
+	            }
+	            else if(num>check)
+	            {
+	                end--;
+	            }
+	            else
+	            {
+	                low++;
+	            }
+	        }
+	    }
+	    return false;
+
+		//////////////// or ///////////////////
+		
 	    sort(arr,arr+n);
 	    for(int i=2;i<n;i++)
 	    {
@@ -30,5 +58,6 @@ public:
 	    }
 	    return false;
 	    // code here
+	
 	}
 };
