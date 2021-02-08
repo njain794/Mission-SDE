88ee904c4963dc56308fe59ea0ab13eb132289d5
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-08 12:42:45 +0530

    Update *****Clone a linked list with next and random pointer.cpp

--- Linked List/*****Clone a linked list with next and random pointer.cpp
+++ Linked List/*****Clone a linked list with next and random pointer.cpp
@@ -11,3 +11,41 @@ Node *copyList(Node *head)
     // Your code here
 }
 ////////// This is only a hack for coding round not to be used for interviews /////////////////////
+
+Node *copyList(Node *head) {
+    // Your code here
+    Node *temp=head;
+    unordered_map<Node*,Node*>mp;
+    
+    while(temp!=NULL)
+    {
+        mp[temp]=new Node(temp->data);
+        temp=temp->next;
+    }
+    
+    Node *curr=head;
+    Node *hg=curr;
+    
+    while(curr!=NULL)
+    {
+        if(mp[curr->next]==NULL)
+        {
+            mp[curr]->next=NULL;
+        }
+        else
+        {
+            mp[curr]->next=mp[curr->next];
+        }    
+        if(mp[curr->arb]==NULL)
+        {
+            mp[curr]->arb=NULL;
+        }
+        else
+        {
+            mp[curr]->arb=mp[curr->arb];
+        }
+        curr=curr->next;
+    }
+    return mp[head];
+    
+}
