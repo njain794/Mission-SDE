da81245d7f688f316b3dd5c0a9267a5f458e52f0
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-09 18:26:24 +0530

    Create >>>(Linear DP)Perfect Squares.cpp

---
+++ DP - 2/>>>(Linear DP)Perfect Squares.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        if(n<=3)
+        {
+            return n;
+        }
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        
+        for(int i=4;i<=n;i++)
+        {
+            dp[i]=INT_MAX;
+        }
+        
+        for(int i=4;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                int temp=j*j;
+                if(temp>n)
+                {
+                    break;
+                }
+                dp[i]=min(dp[i],1+dp[i-temp]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
