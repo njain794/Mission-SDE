788d7c69ac91db1218d02ccd5acdf9b097cf9a69
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-17 20:16:49 +0530

    Update >>>Count the number of ways to tile the floor of size n x m using 1 x m size tiles.cpp

--- DP - 2/>>>Count the number of ways to tile the floor of size n x m using 1 x m size tiles.cpp
+++ DP - 2/>>>Count the number of ways to tile the floor of size n x m using 1 x m size tiles.cpp
@@ -9,3 +9,30 @@ int countWays(int n, int m)
 	            return (countWays(n-1,m) + countWays(n-m,m));
 	        }
       }    
+
+#define mod 1000000007;
+class Solution{
+	public:
+		int countWays(int n, int m)
+	    {
+	        if(n<m)
+	        {
+	            return 1;
+	        }
+	        int dp[n+1];
+	        dp[0]=1;
+	        for(int i=1;i<=n;i++)
+	        {
+	            if(i-m>=0)
+	            {
+	                dp[i]=(dp[i-1]+dp[i-m])%mod;
+	            }
+	            else
+	            {
+	                dp[i]=dp[i-1];
+	            }
+	        }
+	        return dp[n];
+	       // Code here.
+	    }
+};
