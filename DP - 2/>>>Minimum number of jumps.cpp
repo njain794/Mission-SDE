51fd113c29e54dfdc7c954cdeb51da958231e963
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-18 10:59:10 +0530

    Update >>>Minimum number of jumps.cpp

--- DP - 2/>>>Minimum number of jumps.cpp
+++ DP - 2/>>>Minimum number of jumps.cpp
@@ -1,6 +1,41 @@
 // Function to return minimum number of jumps to end of array
 int minJumps(int arr[], int n)
 {
+    //O(n) method
+    if(arr[0]==0)
+    {
+        return -1;
+    }
+    if(n==1)
+    {
+        return 0;
+    }
+    int jumps=0,currend=0,farthest=0;
+    for(int i=0;i<n-1;i++)
+    {
+        farthest=max(farthest,arr[i]+i);
+        if(farthest>=n-1)
+        {
+            return 1+jumps;
+        }
+        if(i==currend)
+        {
+            jumps++;
+            currend=farthest;
+        }
+        if(currend>=n-1)
+        {
+            break;
+        }
+    }
+    if(currend<n-1)
+    {
+        return -1;
+    }
+    return jumps;
+    
+    //O(n^2) - method
+    
     if(n==0)
     {
         return 0;
