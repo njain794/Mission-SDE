e00cb7e59e5168b3b93f9383aa8733b47297c7a3
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-02-20 13:41:45 +0530

    Update insert and search.cpp

--- Tries/insert and search.cpp
+++ Tries/insert and search.cpp
@@ -1,89 +1,92 @@
+
+// C++ implementation of search and insert
+// operations on Trie
 #include <bits/stdc++.h>
 using namespace std;
 
-class node
-{
-    public: char data;
-            unordered_map<char,node*>children;
-            bool terminal;
+const int ALPHABET_SIZE = 26;
 
-            node(char d)
-            {
-                data = d;
-                terminal = false;
-            }
+// trie node
+struct TrieNode
+{
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isEndOfWord is true if the node represents
+    // end of a word
+    bool isEndOfWord;
 };
 
-class trie
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void)
 {
-    private :
-        node *root;
-        int count;
+    struct TrieNode *pNode =  new TrieNode;
 
-    public:
-    trie()
-    {
-        root = new node('\0');
-        count = 0;
-    }
+    pNode->isEndOfWord = false;
 
-    void insert(char *w)
-    {
-        node *temp = root;
-        for(int i=0;w[i]!='\0';i++)
-        {
-            char ch = w[i];
-            if(temp->children.count(ch))
-            {
-                temp = temp->children[ch];
-            }
-            else
-            {
-                node *n = new node(ch);
-                temp->children[ch]=n;
-                temp=n;
-            }
-        }
-        temp->terminal = true;
-    }
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
 
-    bool findo(char *w)
-    {
-        node *temp = root;
-        for(int i=0;w[i]!='\0';i++)
-        {
-            char ch = w[i];
-            if(temp->children.count(ch)==0)
-            {
-                return false;
-            }
-            else
-            {
-                temp = temp->children[ch];
-            }
-        }
-        return temp->terminal;
-    }
-};
+    return pNode;
+}
 
-int main() 
+// If not present, inserts key into trie
+// If the key is prefix of trie node, just
+// marks leaf node
+void insert(struct TrieNode *root, string key)
 {
-    trie t;
-    char word[][10] = {"a","not","hello","news","apple"};
-    char w[10];
-    cin>>w;
+    struct TrieNode *pCrawl = root;
 
-    for(int i=0;i<5;i++)
+    for (int i = 0; i < key.length(); i++)
     {
-        t.insert(word[i]);
+        int index = key[i] - 'a';
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+
+        pCrawl = pCrawl->children[index];
     }
 
-    if(t.findo(w))
+    // mark last node as leaf
+    pCrawl->isEndOfWord = true;
+}
+
+// Returns true if key presents in trie, else
+// false
+bool search(struct TrieNode *root, string key)
+{
+    struct TrieNode *pCrawl = root;
+
+    for (int i = 0; i < key.length(); i++)
     {
-        cout<<"Present";
+        int index = key[i] - 'a';
+        if (!pCrawl->children[index])
+            return false;
+
+        pCrawl = pCrawl->children[index];
     }
-    else
-    {
-        cout<<"Absent";
-    }
+
+    return (pCrawl != NULL && pCrawl->isEndOfWord);
+}
+
+// Driver Code
+int main()
+{
+    // Input keys (use only 'a' through 'z'
+    // and lower case)
+    string keys[] = {"the", "a", "there",
+                    "answer", "any", "by",
+                     "bye", "their" };
+    int n = sizeof(keys)/sizeof(keys[0]);
+
+    struct TrieNode *root = getNode();
+
+    // Construct trie
+    for (int i = 0; i < n; i++)
+        insert(root, keys[i]);
+
+    // Search for different keys
+    search(root, "the")? cout << "Yes\n" :
+                         cout << "No\n";
+    search(root, "these")? cout << "Yes\n" :
+                           cout << "No\n";
+    return 0;
 }
