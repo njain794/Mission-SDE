7bf7b4e02eb6336818e2f7ba6871f7cb3d8a79b7
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-04-01 21:01:05 +0530

    Update ***Minimum Platforms.cpp

--- sorting/***Minimum Platforms.cpp
+++ sorting/***Minimum Platforms.cpp
@@ -23,4 +23,29 @@ int findPlatform(int arr[], int dep[], int n)
     }
     return maxp;
 	// Your code here
+	/*multimap<int,char>mp;
+    char ch1='a';
+    char ch2='d';
+    for(int i=0;i<n;i++)
+    {
+        //mp[arr[i]]=ch1;
+        //mp[dep[i]]=ch2;
+        mp.insert({arr[i],ch1});
+        mp.insert({dep[i],ch2});
+    }
+    int ans=0;
+    int res=INT_MIN;
+    for(auto x:mp)
+    {
+        if(x.second==ch1)
+        {
+            ans++;
+        }
+        else //if(x.second==ch2)
+        {
+            ans--;
+        }
+        res=max(res,ans);
+    }
+    return res;*/
 }
