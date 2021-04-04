60ae29d2be823b9c9b94b806777a7592a7dcc874
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-04-04 10:57:13 +0530

    Update >>> Count ways to N'th Stair(Order does not matter) .cpp

--- DP - 2/>>> Count ways to N'th Stair(Order does not matter) .cpp
+++ DP - 2/>>> Count ways to N'th Stair(Order does not matter) .cpp
@@ -9,10 +9,17 @@ long long countWays(int m)
         dp[i]=1+dp[i-2];
     }
     return dp[m];
+    
+    //////////OR/////////////
+    
     /*if(m==0 or m==1)
     {
         return 1;
     }
     return 1+countWays(m-2);*/
+    
+    //////////OR//////////
+    
+    // return (n/2)+1;
     // your code here
 }
