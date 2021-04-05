187b275730e4f146e20e71cd1f111f312a776cd5
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-04-06 00:11:05 +0530

    Update Steps by Knight.cpp

--- Graph/Steps by Knight.cpp
+++ Graph/Steps by Knight.cpp
@@ -1,74 +1,79 @@
-////****Not Submitted***/////
 class Solution {
 public:
-
-    void expnbr(int x,int y,int d1,int d2,queue<pair<int,int>>&q,vector<bool>vis[],bool &flag,int n)
-    {
-        int cx[]={-1,-1,1,1,2,2,-2,-2};
-        int cy[]={2,-2,2,-2,1,-1,1,-1};
-        
-        for(int i=0;i<8;i++)
-        {
-            if(x+cx[i]<0 or x+cx[i]>=n or y+cy[i]<0 or y+cy[i]>=n or vis[x+cx[i]][y+cy[i]])
-            {
-                continue;
-            }
-            if(x+cx[i]==d1 and y+cy[i]==d2)
-            {
-                flag=true;
-                return;
-            }
-            q.push({x+cx[i],y+cy[i]});
-            vis[x+cx[i]][y+cy[i]]=true;
-        }
-    }
-
 	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int n)
 	{
-	    int steps=0;
-	    int s1=KnightPos[0]-1;
-	    int s2=KnightPos[1]-1;
+	    int x1=KnightPos[0];
+	    int y1=KnightPos[1];
 	    
-	    int d1=TargetPos[0]-1;
-	    int d2=TargetPos[1]-1;
+	    int x2=TargetPos[0];
+	    int y2=TargetPos[1];
 	    
-	    if(s1==d1 and s2==d2)
+	    if(x1==x2 and y1==y2)
 	    {
 	        return 0;
 	    }
-	    queue<pair<int,int>>q;
-	    vector<bool>vis[n];
-	    for(int r=0;r<n;r++)
-	    {
-	        for(int c=0;c<n;c++)
-	        {
-	            vis[r].push_back(false);
-	        }
-	    }
-	    q.push({s1,s2});
-	    vis[s1][s2]=true;
-	    int count=q.size();
-	    bool flag=false;
 	    
-	    while(count>0)
+	    int mat[1001][1001];
+	    for(int i=0;i<n;i++)
 	    {
-	        steps++;
-	        for(int i=0;i<n;i++)
+	        for(int j=0;j<n;j++)
 	        {
-    	        pair<int,int>p=q.front();
-    	        q.pop();
-    	        expnbr(p.first,p.second,d1,d2,q,vis,flag,n);
-    	        
-    	        if(flag)
-    	        {
-    	            return steps;
-    	        }
-    	        //steps++;
+	            mat[i][j]=0;
 	        }
-	        //steps++;
-	        count=q.size();
 	    }
-	    return -1;
+	    
+	    queue<pair<int,int>>q;
+	    q.push({x1-1,y1-1});
+	    while(!q.empty())
+	    {
+	        auto curr=q.front();
+	        int i=curr.first;
+	        int j=curr.second;
+	        q.pop();
+	        
+	        if(i-2>=0 and i-2<n and j+1>=0 and j+1<n and mat[i-2][j+1]==0)
+	        {
+	            mat[i-2][j+1]=mat[i][j]+1;
+	            q.push({i-2,j+1});
+	        }
+	        if(i-2>=0 and i-2<n and j-1>=0 and j-1<n and mat[i-2][j-1]==0)
+	        {
+	            mat[i-2][j-1]=mat[i][j]+1;
+	            q.push({i-2,j-1});
+	        }
+	        if(i+2>=0 and i+2<n and j+1>=0 and j+1<n and mat[i+2][j+1]==0)
+	        {
+	            mat[i+2][j+1]=mat[i][j]+1;
+	            q.push({i+2,j+1});
+	        }
+	        if(i+2>=0 and i+2<n and j-1>=0 and j-1<n and mat[i+2][j-1]==0)
+	        {
+	            mat[i+2][j-1]=mat[i][j]+1;
+	            q.push({i+2,j-1});
+	        }
+	        if(i-1>=0 and i-1<n and j+2>=0 and j+2<n and mat[i-1][j+2]==0)
+	        {
+	            mat[i-1][j+2]=mat[i][j]+1;
+	            q.push({i-1,j+2});
+	        }
+	        if(i-1>=0 and i-1<n and j-2>=0 and j-2<n and mat[i-1][j-2]==0)
+	        {
+	            mat[i-1][j-2]=mat[i][j]+1;
+	            q.push({i-1,j-2});
+	        }
+	        if(i+1>=0 and i+1<n and j+2>=0 and j+2<n and mat[i+1][j+2]==0)
+	        {
+	            mat[i+1][j+2]=mat[i][j]+1;
+	            q.push({i+1,j+2});
+	        }
+	        if(i+1>=0 and i+1<n and j-2>=0 and j-2<n and mat[i+1][j-2]==0)
+	        {
+	            mat[i+1][j-2]=mat[i][j]+1;
+	            q.push({i+1,j-2});
+	        }
+	    }
+	    
+	    return mat[x2-1][y2-1];
 	    // Code here
 	}
 };
