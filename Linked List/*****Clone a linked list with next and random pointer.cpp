8f687876ded4489f37a0c550892a62f8383d0901
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-05-22 20:44:20 +0530

    Update *****Clone a linked list with next and random pointer.cpp

--- Linked List/*****Clone a linked list with next and random pointer.cpp
+++ Linked List/*****Clone a linked list with next and random pointer.cpp
@@ -1,5 +1,61 @@
 Node *copyList(Node *head) 
 {
+    /*if(head==NULL or head->next==NULL)
+    {
+        return head;
+    }*/
+    
+    Node *curr=head;
+    Node *tmp;
+    
+    while(curr)
+    {
+        tmp=curr->next;
+        curr->next=new Node(curr->data);
+        curr->next->next=tmp;
+        curr=tmp;
+    }
+    
+    curr=head;
+    while(curr)
+    {
+        if(curr->arb)
+        {
+            curr->next->arb=curr->arb->next;
+        }
+        else
+        {
+            curr->next->arb=curr->arb;
+        }
+        curr=curr->next->next;
+    }
+    
+    Node *orig=head;
+    Node *copy=head->next;
+    Node *temp=copy;
+    
+    while(orig and copy)
+    {
+        orig->next=orig->next->next;
+        if(copy->next)
+        {
+            copy->next=copy->next->next;
+        }
+        else
+        {
+            copy->next=copy->next;
+        }
+        orig=orig->next;
+        copy=copy->next;
+    }
+    
+    return temp;
+    // Your code here
+}
+
+
+Node *copyList(Node *head) 
+{
     if(head==NULL)
     {
         return NULL;
