850d1602ae4b2d01f8247331ef54a4dfd8a98e21
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-06-28 10:55:46 +0530

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ DP - 2/Buy And Sell Stock/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        int profit=0;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i-1]<arr[i])
+            {
+                profit+=arr[i]-arr[i-1];
+            }
+        }
+        
+        return profit;
+    }
+};
