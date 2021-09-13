c9f7f5425f39f0846be0e08e25c6c560c559228e
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-09-13 16:38:14 +0530

    Update Consecutive 1's not allowed.cpp

--- DP-3/Consecutive 1's not allowed.cpp
+++ DP-3/Consecutive 1's not allowed.cpp
@@ -1,40 +1,27 @@
-class Solution {
-public:
-    int dx[4]={0,0,1,-1};
-    int dy[4]={1,-1,0,0};
-    
-    int solve(vector<vector<int>> &grid,int n,int m,int i,int j)
-    {
-        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]==0)
-        {
-            return 0;
-        }
-        int val=grid[i][j];
-        grid[i][j]=0;
-        int mx=0;
-        
-        for(int k=0;k<4;k++)
-        {
-            mx=max(mx,solve(grid,n,m,i+dx[k],j+dy[k]));
-        }
-        grid[i][j]=val;
-        return val+mx;
-    }
-    
-    int getMaximumGold(vector<vector<int>>& grid) 
-    {
-        int n=grid.size();
-        int m=grid[0].size();
-        
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                int temp=solve(grid,n,m,i,j);
-                ans=max(ans,temp);
-            }
-        }
-        return ans;
-    }
-};
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+	int no ;
+
+	cin >> no ;
+
+	int oldOnes = 1 ;
+	int oldZeros = 1 ;
+
+	for( int i = 2 ; i <= n ; i++ )
+	{
+		int newOnes = oldZeros+oldOnes ;
+		int newzeros = oldOnes ;
+
+		oldOnes = newOnes ;
+		oldZeros = newzeros ;
+	}
+
+	cout<< oldZeros+oldOnes ;
+
+
+	return 0 ;
+}
