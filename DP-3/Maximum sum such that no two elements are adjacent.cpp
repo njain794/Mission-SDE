1346088613bc1e257ebcc9367c09a9ce5b1d0654
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2021-09-15 12:24:58 +0530

    Update Maximum sum such that no two elements are adjacent.cpp

--- DP-3/Maximum sum such that no two elements are adjacent.cpp
+++ DP-3/Maximum sum such that no two elements are adjacent.cpp
@@ -1,1 +1,14 @@
 //https://practice.geeksforgeeks.org/viewSol.php?subId=ce677628f41e1a89aad8391995a5c5b1&pid=701417&user=namanjain41
+
+int incl=arr[0];
+int excl=0;
+
+for(int i=1;i<n;i++)
+{
+    int a=excl+arr[i];
+    int b=max(incl,excl);
+
+    incl=a;
+    excl=b;
+}
+return max(incl,excl);
