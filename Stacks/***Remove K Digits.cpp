d3f9c3ac803e4d0139ef3493e56c4301c2aa6949
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2022-06-11 22:16:37 +0530

    Update ***Remove K Digits.cpp

--- Stacks/***Remove K Digits.cpp
+++ Stacks/***Remove K Digits.cpp
@@ -1,1 +1,41 @@
 https://practice.geeksforgeeks.org/problems/remove-k-digits/1#
+
+class Solution{
+    public:
+    string removeKdigits(string str, int k) 
+    {
+        stack<char>st;
+        string ans;
+        
+        for(auto c:str)
+        {
+            while(!st.empty() and k>0 and st.top()>c)
+            {
+                st.pop();
+                k--;
+            }
+            if(!st.empty() or c!='0')
+            {
+                st.push(c);
+            }
+        }
+        
+        while(!st.empty() and k--)
+        {
+            st.pop();
+        }
+        if(st.empty())
+        {
+            return "0";
+        }
+        
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
