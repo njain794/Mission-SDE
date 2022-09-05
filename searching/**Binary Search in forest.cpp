feadc83e63fb30618ccbf824099230f919e98d41
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2022-09-05 13:57:38 +0530

    Update **Binary Search in forest.cpp

--- searching/**Binary Search in forest.cpp
+++ searching/**Binary Search in forest.cpp
@@ -1,3 +1,51 @@
+int canCut(int tree[], int n, int k, int mid)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(tree[i]>mid)
+            {
+                sum+=tree[i]-mid;
+            }
+        }
+        return sum;
+    }
+    
+    int find_height(int tree[], int n, int k)
+    {
+        if(n==1 && tree[0]>k)
+        {
+            return tree[0]-k;
+        }
+        sort(tree,tree+n);
+        
+        int low=tree[0];
+        int hii=tree[n-1];
+        int ans=-1;
+        
+        while(low<=hii)
+        {
+            int mid=low+(hii-low)/2;
+            if(canCut(tree,n,k,mid)==k)
+            {
+                
+                ans=mid;
+                low=mid+1;
+            }
+            else if(canCut(tree,n,k,mid)>k)
+            {
+                low=mid+1;
+            }
+            else{
+                hii=mid-1;
+            }
+        }
+        return ans;
+    }
+
+
+**********************************************************
+
 int find_height(int tree[], int n, int k)
     {
         sort(tree,tree+n);
