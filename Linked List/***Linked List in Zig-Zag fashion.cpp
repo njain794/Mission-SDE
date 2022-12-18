81514c3fab7546fae0e2963fe7c226d752a58e08
NAMAN JAIN <44344547+njain794@users.noreply.github.com>
2022-12-18 13:51:01 +0530

    Update ***Linked List in Zig-Zag fashion.cpp

--- Linked List/***Linked List in Zig-Zag fashion.cpp
+++ Linked List/***Linked List in Zig-Zag fashion.cpp
@@ -1,26 +1,28 @@
 Node *zigZag(Node* head)
 {
-Node* temp=head;
-Node* c=temp->next;
+      Node* temp=head;
+      Node* c=temp->next;
 
-while(temp && c )
-{
-if(temp->data>c->data)
-{ swap(temp->data,c->data);
-}
-temp=temp->next;
-c=c->next;
-if(c==NULL)
-{
-break;
-}
+      while(temp && c )
+      {
+          if(temp->data>c->data)
+          { 
+              swap(temp->data,c->data);
+          }
+          temp=temp->next;
+          c=c->next;
+        
+          if(c==NULL)
+          {
+              break;
+          }
 
-if(temp->data<c->data)
-{
-swap(temp->data,c->data);
-}
-temp=temp->next;
-c=c->next;
-}
-return head;
+          if(temp->data<c->data)
+          {
+              swap(temp->data,c->data);
+          }
+          temp=temp->next;
+          c=c->next;
+      }
+      return head;
 }
